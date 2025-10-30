c36396e9e20acacc4b5f7394806abd8ce66ce629
WolodiaM <w_melnyk@outlook.com>
2025-10-30 20:59:52 +0200

    .dir-locals.el: Updated compile flags

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,7 @@
 ((simpc-mode . ((simpc-flymake-overrides
 								 . (
 										("cbuild.h" . (:flags ("-DCBUILD_IMPLEMENTATION")))
-										("tests/*.c" . (:flags ("-include" "../framework.h")))
+										("tests/*.c" . (:flags ("-include" "../framework.h" "-DTEST_RUN_PLATFORM=")))
 										)
 								 ))
 						 ))
