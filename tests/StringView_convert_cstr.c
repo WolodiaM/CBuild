07e2291eb1acf076f298f0aef751fc792314b57d
WolodiaM <w_melnyk@outlook.com>
2026-07-31 21:12:00 +0300

    Fixed convert_cstr test

--- tests/StringView_convert_cstr.c
+++ tests/StringView_convert_cstr.c
@@ -2,7 +2,7 @@ int main(void) {
 	// cbuild_sv_to_cstr
 	cbuild_sv_t sv = cbuild_sv_from_parts("abcdef", 3);
 	const char* reference = "abc";
-	char* cstr1 =	cbuild_sv_to_cstr(sv);
+	char* cstr1 =	cbuild_sv_to_cstr_malloc(sv);
 	TEST_ASSERT_STREQ(reference, cstr1, "Conversion to c-string failed"
 		TEST_EXPECT_MSG(s), reference, cstr1);
 	free(cstr1);
@@ -10,7 +10,7 @@ int main(void) {
 	cbuild_temp_malloc(0);
 	cbuild_temp_reset(0);
 	void* temp_base = __cbuild_temp_arena.base;
-	char* cstr2 = cbuild_sv_to_temp_cstr(sv);
+	char* cstr2 = cbuild_sv_to_cstr_temp(sv);
 	TEST_ASSERT_STREQ(reference, cstr2, "Conversion to c-string failed"
 		TEST_EXPECT_MSG(s), reference, cstr2);
 	TEST_ASSERT_EQ(cstr2, temp_base,
@@ -20,7 +20,7 @@ int main(void) {
 	cbuild_arena_t arena = {0};
 	cbuild_arena_base_malloc(&arena, 1024);
 	void* arena_base = arena.base;
-	char* cstr3 = cbuild_sv_to_arena_cstr(&arena, sv);
+	char* cstr3 = cbuild_sv_to_cstr_arena(&arena, sv);
 	TEST_ASSERT_STREQ(reference, cstr3, "Conversion to c-string failed"
 		TEST_EXPECT_MSG(s), reference, cstr3);
 	TEST_ASSERT_EQ(cstr3, arena_base,
